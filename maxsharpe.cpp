7be683b285dd070faac1c202730b171cd048a018
Lin Gao <lingao.gao@mail.utoronto.ca>
2017-03-03 11:41:04 -0500

    Tested reading x0,x,y.

--- maxsharpe.cpp
+++ maxsharpe.cpp
@@ -4,29 +4,35 @@
 #include <vector>
 #include <string>
 #include <math.h>
+#include <stdlib.h>
 #include <readtxt.h>
 
 int main(int argc, char *argv[])
 {
     // load and check files
-    std::ifstream f("x1.csv");
-    if(!f) {
-        std::cout << "Cannot open input file "<< f << ".\n";
+    std::string x0("x0.csv");
+    std::string x("x.csv");
+    std::string y("y.csv");
+    std::ifstream f_x0(x0.c_str());
+    if(!f_x0) {
+        std::cout << "Cannot open input file "<< x0.c_str() << ".\n";
+        return 1;
+    }
+    std::ifstream f_x(x.c_str());
+    if(!f_x) {
+        std::cout << "Cannot open input file "<< x.c_str() << ".\n";
+        return 1;
+    }
+    std::ifstream f_y(y.c_str());
+    if(!f_y) {
+        std::cout << "Cannot open input file "<< y.c_str() << ".\n";
         return 1;
     }
 
     // read data 
-    int N_row;
-    int N_col;
-    std::vector<std::vector<double> > data = readtxt("x1.csv",N_row,N_col);
-    std:: cout<< "Size of data is "<<N_row<<" rows, "<<N_col<< " cols.\n";
+    int N_x0_row;
+    int N_x0_col;
+    std::vector<std::vector<double> > data = readtxt(x0,N_x0_row,N_x0_col);
+    std:: cout<< "Size of x0 is "<<N_x0_row<<" rows, "<<N_x0_col<< " cols.\n";
 
-    // output data to verify
-    std::cout << "The data is:\n";
-    for (int i=0;i<N_row;i++){
-        for (int j=0;j<N_col;j++){
-            std::cout << data[i][j] << " ";
-        }
-        std::cout << "\n";
-    } 
 }
