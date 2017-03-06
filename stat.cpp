7799bc2086e149af89f67904e9d53eed3a8dd06b
Lin Gao <lingao.gao@mail.utoronto.ca>
2017-03-06 10:55:15 -0500

    Added the output in stat.cpp

--- stat.cpp
+++ stat.cpp
@@ -10,19 +10,45 @@
 #include <stat.h>
 
 std::vector<std::vector<double> > simplestat(const std::vector<std::vector<double> >& data, int N){
-    // get stat data
-    // mean and std on each dim (for now)
+    // compute and output mean,std,skew and kurt on each dim
     std::vector<std::vector<double> > stat;
+
+    // using boost accumulators library
     using namespace boost::accumulators;
-    accumulator_set<double, features<tag::mean, tag::variance(lazy), tag::skewness, tag:: kurtosis> > acc;
+    accumulator_set<double, features<tag::mean, tag::variance(lazy), tag::skewness, tag::kurtosis> > acc;
     for (int i=0;i<N;i++){
-        std::for_each(data[i].begin(), data[i].end(), boost::bind<void>(boost::ref(acc),boost::lambda::_1));
-        std::vector<double> stat_1D;
-        stat_1D.push_back(boost::accumulators::mean(acc));
+        std::vector<double> stat_1D;  // initialize stat on each dim
+
+        std::for_each(data[i].begin(), data[i].end(), boost::bind<void>(boost::ref(acc),boost::lambda::_1)); // put data on each dim into acc
+
+        stat_1D.push_back(boost::accumulators::mean(acc)); // store stats of acc in stat_1D
         stat_1D.push_back(sqrt(boost::accumulators::variance(acc)));
-        stat_1D.push_back(sqrt(boost::accumulators::skewness(acc)));
-        stat_1D.push_back(sqrt(boost::accumulators::kurtosis(acc)));
-        stat.push_back(stat_1D);
+        stat_1D.push_back(boost::accumulators::skewness(acc));
+        stat_1D.push_back(boost::accumulators::kurtosis(acc));
+
+        stat.push_back(stat_1D); // store stat_1D into stat
+    }
+
+    // print out the stats
+    std::cout << "Statistics:" << std::endl;
+    std::cout << " Mean ";
+    for (int i=0;i<N;i++){
+        std::cout << stat[i][0] << ",";
+    }
+    std::cout << std::endl;
+    std::cout << " Std ";
+    for (int i=0;i<N;i++){
+        std::cout << stat[i][1] << ",";
+    }
+    std::cout << std::endl;
+    std::cout << " Skewness ";
+    for (int i=0;i<N;i++){
+        std::cout << stat[i][2] << ",";
+    }
+    std::cout << std::endl;
+    std::cout << " Kurtosis ";
+    for (int i=0;i<N;i++){
+        std::cout << stat[i][3] << ",";
     }
     
     return stat;
